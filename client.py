6698934a578b94858df497da00ce39b1b8d7b448
Eduardo Imery <ejwimery@gmail.com>
2019-09-08 21:06:49 -0600

    defininf function init

--- client.py
+++ client.py
@@ -2,4 +2,16 @@ import socket
 import _pickle as pickle
 
 class Network:
-    pass
+    """
+    class to connect, send and recive information from server
+    need to hardcode the host attribute to be the server's ip
+    """
+
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #the host ip addrees
+        self.host = "192.168.1.12"
+        #the connection port 
+        self.port = 5555
+        self.addr = (self.host, self.port)
+        
